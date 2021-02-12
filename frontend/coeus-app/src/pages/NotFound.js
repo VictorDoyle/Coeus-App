93abcaf611a43bf4b039ed71a8c9c35bd2c0dacf
victordoyle <victor-doyle@outlook.com>
2021-02-12 11:18:08 -0500

    styled 404 page

--- frontend/coeus-app/src/pages/NotFound.js
+++ frontend/coeus-app/src/pages/NotFound.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/LandingPage.css'
+import '../styles/NotFound.css'
 import {  Button, Card } from 'semantic-ui-react'
 
 
