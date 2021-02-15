6385c7eacdab9c89737b5ac9b112e8b132b69d43
victordoyle <victor-doyle@outlook.com>
2021-02-15 16:11:43 -0500

    adding env file to frontendapi

--- frontend/coeus-app/src/models/news.js
+++ frontend/coeus-app/src/models/news.js
@@ -1,11 +1,12 @@
-/* import NewsApiKey from '../testing/env.js'
+import dotenv from "dotenv"
 
- */
+dotenv.config();
+
 
 class NewsModel {
 
 static getWeeklyNews = () => {
-    return fetch(`https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=${NewsApiKey}`, {
+    return fetch(`https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=${process.env.newsapikey}`, {
         method: "GET"
         }).then(res => res.json())
     }
