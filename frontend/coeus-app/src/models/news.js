3e6990c6ef6c41da69e1379c8395e292056152a5
victordoyle <victor-doyle@outlook.com>
2021-02-16 21:27:12 -0500

    fixing news api

--- frontend/coeus-app/src/models/news.js
+++ frontend/coeus-app/src/models/news.js
@@ -1,8 +1,8 @@
-import dotenv from "dotenv"
+/* import dotenv from "dotenv"
 
 dotenv.config(); 
-
-const newsAPI = process.env.NewsApiKey
+ */
+const newsAPI = process.env.REACT_APP_NewsApiKey
 
 class NewsModel {
 
